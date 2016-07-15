64c1ee570f7cf852998a30df20c506714a478e44
ThePrincessOf69 <sebyakin.2332.sergei@yandex.ru>
2016-07-15 10:48:26 +0300

    *

---
+++ parse_what_i_know.py
@@ -0,0 +1,22 @@
+import wikilib
+import categoryLister as lister
+import subprocess
+
+
+def has_coord(page):
+    process = subprocess.Popen(['coorder.pl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    print(process.communicate(input=page.encode('UTF-8')))
+    return True
+    #return process.communicate(input=page)[0] != ''
+
+
+def parse(page):
+    res = wikilib.parse_infobox_military_conflict(wikilib.get_infobox(page)[0])
+    return (res != '') and ('date' in res) and has_coord(page)
+
+f = open('logs/page_list.txt')
+lst = f.readlines()
+for s in lst:
+    s = s.strip()
+    if parse(lister.cachingGetPage(s)):
+        print(s)

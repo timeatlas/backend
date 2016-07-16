e16fa96611980d05838389dc9e35107d6834a5de
AgentBrown <s17b2_kozincev@179.ru>
2016-07-16 10:53:33 +0300

    Changed work method for date7000

---
+++ dateConverters.py
@@ -0,0 +1,15 @@
+def cf(date):
+	year = date.year
+	if year > 7000:
+		year -= 7000
+	else:
+		year -= 7001
+	return (year, date.month, date.day)
+
+def ct(date):
+	year = date[0]
+	if year > 0:
+		year += 7000
+	else:
+		year += 7001
+	return datetime.date(year, date[1], date[2])

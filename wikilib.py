1b9ba714366ee50db89a736783cf2886a9f84faf
VorontsovIE <vorontsov.i.e@gmail.com>
2016-07-20 18:12:58 +0300

    fix for has_infobox

--- wikilib.py
+++ wikilib.py
@@ -24,7 +24,7 @@ def get_infobox(page): # Returns infobox
 
 def has_infobox(page, infobox_type):
     infobox = get_infobox(page)
-    if (infobox == '') or (infobox.lower() != infobox_type.lower()):
+    if (infobox == '') or (wiki_template.parse_template(infobox)['template_name'].lower() != infobox_type.lower()):
         return False
     else:
         return True
